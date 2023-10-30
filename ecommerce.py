400200b3cdeb148fe72b505818724392d402b27d
UrielLiannLima <126467176+UrielLiannLima@users.noreply.github.com>
2023-10-30 20:12:41 -0300

    Especializações adicionadas

--- ecommerce.py
+++ ecommerce.py
@@ -13,3 +13,19 @@ class Produto:
         self.nome = nome
         self.preco = preco
         self.marca = marca
+        
+#Classes especializadas herdando de Produto, cada uma com um atributo único para si
+class Livros(Produto):
+    def __init__(self, codigoBarras, nome, preco, marca, autor):
+        super().__init__(codigoBarras, nome, preco, marca)
+        self.autor = autor
+
+class DVD(Produto):
+    def __init__(self, codigoBarras, nome, preco, marca, diretor):
+        super().__init__(codigoBarras, nome, preco, marca)
+        self.diretor = diretor
+
+class Vinil(Produto):
+    def __init__(self, codigoBarras, nome, preco, marca, musica):
+        super().__init__(codigoBarras, nome, preco, marca)
+        self.musica = musica
