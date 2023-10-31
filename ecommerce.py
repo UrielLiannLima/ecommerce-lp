a48d00dc7c94f2131218c06ca79e517e6b3b9f3d
UrielLiannLima <126467176+UrielLiannLima@users.noreply.github.com>
2023-10-30 21:23:06 -0300

    implementação do estoque e criação dos

--- ecommerce.py
+++ ecommerce.py
@@ -53,3 +53,61 @@ class Vinil(Produto):
     def __init__(self, codigoBarras, nome, preco, marca, musica):
         super().__init__(codigoBarras, nome, preco, marca)
         self.musica = musica
+
+#Classe Estoque que gerencia o estoque de produtos
+class Estoque:
+    def __init__(self):
+        self.produtos = {}  #Dicionário para manter o estoque
+
+    def adicionarProduto(self, produto, quantidade):
+        if quantidade <= 0:
+            raise ValueError("A quantidade deve ser maior que zero.")
+        
+        if produto.getCodigoBarras() in self.produtos:
+            self.produtos[produto.getCodigoBarras()] += quantidade
+        else:
+            self.produtos[produto.getCodigoBarras()] = quantidade
+
+    def venderProduto(self, codigoBarras, quantidade):
+        if codigoBarras not in self.produtos:
+            raise Exception("Produto não está no estoque.")
+        
+        if self.produtos[codigoBarras] < quantidade:
+            raise Exception("Quantidade insuficiente para venda.")
+        
+        self.produtos[codigoBarras] -= quantidade
+
+    def devolverProduto(self, codigoBarras, quantidade):
+        if quantidade <= 0:
+            raise ValueError("A quantidade deve ser maior que zero.")
+        
+        if codigoBarras not in self.produtos:
+            raise Exception("Produto não está no estoque.")
+        
+        self.produtos[codigoBarras] += quantidade
+
+    def reabastecerProduto(self, codigoBarras, quantidade):
+        if quantidade <= 0:
+            raise ValueError("A quantidade deve ser maior que zero.")
+        
+        if codigoBarras in self.produtos:
+            self.produtos[codigoBarras] += quantidade
+        else:
+            raise Exception("Produto não encontrado para reabastecimento.")
+
+    def mostrarEstoque(self):
+        print("\nEstado atual do estoque:")
+        for codigoBarras, quantidade in self.produtos.items():
+            print(f"Código de Barras: {codigoBarras}, Quantidade: {quantidade}")
+            
+#Exemplo de uso:
+estoque = Estoque()
+
+# Adicionar produtos ao estoque
+produto1 = Livros(1, "Uma Questão de Química", 31, MarcaProduto.Intriseca, "Isabelli Cristhini")
+produto2 = DVD(2, "A Era do Gelo", 10, MarcaProduto.SONY, "João Otávio")
+produto3 = Vinil(3, "Tequila", 69, MarcaProduto.Tarkett, "Uriel L.")
+
+estoque.adicionarProduto(produto1, 13)
+estoque.adicionarProduto(produto2, 22)
+estoque.adicionarProduto(produto3, 420)
