c14c66eeb062b5a7c3cea216c3647019586348b7
UrielLiannLima <126467176+UrielLiannLima@users.noreply.github.com>
2023-10-30 21:19:26 -0300

    Implementação dos métodos getter e setter

--- ecommerce.py
+++ ecommerce.py
@@ -6,12 +6,36 @@ class MarcaProduto(Enum):
     SONY = 2
     Tarkett = 3
 
-#Classe base Produto com o método construtor
+#Classe base Produto com o método construtor, com os métodos getter e setter para cada atributo
 class Produto:
     def __init__(self, codigoBarras, nome, preco, marca):
         self.codigoBarras = codigoBarras
         self.nome = nome
         self.preco = preco
+        self.marca = marca
+    
+    def getCodigoBarras(self):
+        return self.codigoBarras
+
+    def setCodigoBarras(self, codigoBarras):
+        self.codigoBarras = codigoBarras
+
+    def getNome(self):
+        return self.nome
+
+    def setNome(self, nome):
+        self.nome = nome
+
+    def getPreco(self):
+        return self.preco
+
+    def setPreco(self, preco):
+        self.preco = preco
+
+    def getMarca(self):
+        return self.marca
+
+    def setMarca(self, marca):
         self.marca = marca
         
 #Classes especializadas herdando de Produto, cada uma com um atributo único para si
